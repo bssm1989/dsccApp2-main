f014704e97521f073f0b5bce67cd3298b346e07f
bssm1989 <bssm1989@gmail.com>
2022-07-31 17:24:54 +0700

    add adress

---
+++ Pages/Adress.razor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using FirstBlazorApp.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+
+namespace FirstBlazorApp.Pages
+{
+    public partial class Adress : ComponentBase
+    {
+        List<province> provinces1 = new List<province>();
+        List<district> districts1 = new List<district>();
+        List<district> districts = new List<district>();
+        List<district> districts2 = new List<district>();
+        List<tambon> tambons1 = new List<tambon>();
+        public Adress()
+        {
+        }
+        protected override async Task OnInitializedAsync()
+        {
+            await DBContext.OpenIndexedDb();
+            provinces1 = await DBContext.GetAll<province>("province");
+            districts1 = await DBContext.GetAll<district>("district");
+            tambons1 = await DBContext.GetAll<tambon>("tambon");
+        }
+    }
+}
+
